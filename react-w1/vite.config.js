a3752bae2d8543fc965f98d3317885b5bd78f901
Ray <lhy131120@gmail.com>
2025-01-17 11:01:44 +0800

    update path

--- react-w1/vite.config.js
+++ react-w1/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	// eslint-disable-next-line no-undef
-	base: process.env.NODE_ENV === "production" ? "/react-demo/react-w1/" : "/",
+	base: "/react-demo/react-w1/",
 	plugins: [react()],
 });
--- react-w2/vite.config.js
+++ react-w2/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	// eslint-disable-next-line no-undef
-	base: process.env.NODE_ENV === "production" ? "/react-demo/react-w2/" : "/",
+	base: "/react-demo/react-w2/",
 	plugins: [react()],
 });
